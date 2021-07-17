349681f21de7f18983b0ae76214863d324a39c04
Manuel Tavera <taveras_manuel35@hotmail.com>
2021-07-17 15:31:20 -0400

    Listo para la presentacion del segundo parcial

--- src/common/store/epics/index.js
+++ src/common/store/epics/index.js
@@ -1,44 +1,45 @@
-import { combineEpics } from 'redux-observable';
-import * as userEpics from './user.epic';
-import * as complainsEpic from './complains.epic';
-import * as departmentEpics from './department.epic';
-import * as claimsEpics from './claim.epic';
-import * as answerEpics from './answer.epic';
-import * as productEpics from './products.epic';
+import { combineEpics } from "redux-observable";
+import * as userEpics from "./user.epic";
+import * as complainsEpic from "./complains.epic";
+import * as departmentEpics from "./department.epic";
+import * as claimsEpics from "./claim.epic";
+import * as answerEpics from "./answer.epic";
+import * as productEpics from "./products.epic";
 
 export const rootEpic = combineEpics(
-    userEpics.authUserEpic,
-    userEpics.createUserEpic,
-    userEpics.getAllEmployeesEpic,
-    userEpics.redirectUserFromLoginEpic,
-    complainsEpic.getComplainsEpic,
-    complainsEpic.deleteComplainEpic,
-    complainsEpic.getComplainsTitleEpic,
-    complainsEpic.getByComplainId,
-    complainsEpic.updateComplainsEpic,
-    complainsEpic.createComplainEpic,
-    complainsEpic.createComplainsTypeEpic,
-    complainsEpic.updateComplainsTypeEpic,
-    complainsEpic.deleteComplainsTypeEpic,
-    claimsEpics.getClaimsEpic,
-    claimsEpics.deleteClaimEpic,
-    claimsEpics.getClaimsTitleEpic,
-    claimsEpics.getByClaimIdEpic,
-    claimsEpics.updateClaimsEpic,
-    claimsEpics.createClaimTypeEpic,
-    claimsEpics.updateClaimTypeEpic,
-    claimsEpics.deleteClaimTypeEpic,
-    departmentEpics.getDepartmentsEpic,
-    departmentEpics.createDepartmentEpic,
-    departmentEpics.deleteDepartmentEpic,
-    departmentEpics.updateDepartmentRequest,
-    departmentEpics.getDepartmentByIdRequest,
-    answerEpics.answerClaimComplaintEpic,
-    answerEpics.getAnswersEpic,
-    answerEpics.getClaimsAnswerEpic,
-    answerEpics.getComplaintsAnswerEpic,
-    productEpics.createProductEpic,
-    productEpics.deleteProductEpic,
-    productEpics.getProductsEpic,
-    productEpics.updateProductEpic
+  userEpics.authUserEpic,
+  userEpics.createUserEpic,
+  userEpics.getAllEmployeesEpic,
+  userEpics.redirectUserFromLoginEpic,
+  complainsEpic.getComplainsEpic,
+  complainsEpic.deleteComplainEpic,
+  complainsEpic.getComplainsTitleEpic,
+  complainsEpic.getByComplainId,
+  complainsEpic.updateComplainsEpic,
+  complainsEpic.createComplainEpic,
+  complainsEpic.createComplainsTypeEpic,
+  complainsEpic.updateComplainsTypeEpic,
+  complainsEpic.deleteComplainsTypeEpic,
+  claimsEpics.getClaimsEpic,
+  claimsEpics.deleteClaimEpic,
+  claimsEpics.getClaimsTitleEpic,
+  claimsEpics.getByClaimIdEpic,
+  claimsEpics.updateClaimsEpic,
+  claimsEpics.createClaimTypeEpic,
+  claimsEpics.updateClaimTypeEpic,
+  claimsEpics.deleteClaimTypeEpic,
+  claimsEpics.createEpicClaimEpic,
+  departmentEpics.getDepartmentsEpic,
+  departmentEpics.createDepartmentEpic,
+  departmentEpics.deleteDepartmentEpic,
+  departmentEpics.updateDepartmentRequest,
+  departmentEpics.getDepartmentByIdRequest,
+  answerEpics.answerClaimComplaintEpic,
+  answerEpics.getAnswersEpic,
+  answerEpics.getClaimsAnswerEpic,
+  answerEpics.getComplaintsAnswerEpic,
+  productEpics.createProductEpic,
+  productEpics.deleteProductEpic,
+  productEpics.getProductsEpic,
+  productEpics.updateProductEpic
 );
